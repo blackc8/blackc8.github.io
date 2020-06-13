415182cfd5324f4b7a7bb1c4eeb2ddc87b43fa63
blackc8 <blackc8official@gmail.com>
2020-06-13 08:26:06 +0530

    a

---
+++ res/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="res/style.css">
+    </head>
+    <body>
+        <div id="container">
+            <span id="helo">Hello!</span><br>
+            <span id="iamb">I'm blackc8</span><span class="blinking-cursor">|</span><br><br><br>
+            <span id="desc">Email: blackc8official@gmail.com<br>GitHub: github.com/blackc8</span>
+        </div>
+    </body>
+</html>
